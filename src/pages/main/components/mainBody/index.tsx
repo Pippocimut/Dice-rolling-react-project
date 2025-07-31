83f4356c514efaad8a077f8e5c621e40194fc4a6
PippoCimutThe2nd <51755863+Pippocimut@users.noreply.github.com>
2025-07-31 15:48:05 +0100

    Update index.tsx

--- src/pages/main/components/mainBody/index.tsx
+++ src/pages/main/components/mainBody/index.tsx
@@ -79,7 +79,7 @@ const MainBody = () => {
                 </button>
                 {moreThanOneButton && <button
                     id="editModeButton"
-                    className={"w-40 h-20 rounded-lg hover:outline-2 text-lg" + (editMode ? "bg-white border-4 border-blue-700 text-blue-700" : "bg-blue-800 text-white")}
+                    className={"w-40 h-20 rounded-lg hover:outline-2 text-lg " + (editMode ? " bg-white border-4 border-blue-700 text-blue-700" : " bg-blue-800 text-white")}
                     onClick={() => dispatch(toggleEditMode())}>{
                     editMode ? <span className={"text-lg"}>Done</span> : <div
                         className={"flex justify-center gap-2 items-center"}
